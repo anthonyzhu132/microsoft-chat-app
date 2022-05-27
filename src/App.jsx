cbb55990040d1b36644bac00d538af387eba3d63
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-26 19:07:17 -0700

    adjusted to use .env variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { ChatEngine } from "react-chat-engine";
 import "./styles/App.css";
 
 export const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.PROJECT_ID}
+      userName={process.env.USER_NAME}
+      userSecret={process.env.USER_SECRET}
+    />
+  );
 };
 
 export default App;
