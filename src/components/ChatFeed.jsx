3ea9f8fa7bf51ebb2364162957f571e458289e43
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-26 19:33:44 -0700

    built out new ChatFeed component to show my message, or someone elses message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -23,6 +23,23 @@ const ChatFeed = (props) => {
 
       // Checking if message is own user
       const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+          </div>
+          <div
+            className="read-receipts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            read-receipts
+          </div>
+        </div>
+      );
     });
   };
 
