a7a92d68c1f668cbe780a20879200b4651ae50bc
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-26 22:42:17 -0700

    added check to see if first message is by the right user

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,22 @@
-import React from "react"
+import React from "react";
 
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  // Checks if latest message sent is by the right user
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+
   return (
-    <div>
+    <div className="message-row">
+      {/* Checking if message is by user, render avatar background image */}
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
       Their Message
     </div>
-  )
-}
+  );
+};
 
 export default TheirMessage;
