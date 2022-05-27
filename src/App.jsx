5eec6e1202a3d0dd61d0e940a6e7d9d914850600
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-26 19:00:04 -0700

    added boilerplate and to ignore .env

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./styles/App.css";
+
+export const App = () => {
+  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+};
+
+export default App;
