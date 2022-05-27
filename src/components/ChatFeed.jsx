4bd7c12330a49823f3d5849f89a549db0ab1f166
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-26 19:29:43 -0700

    added variables to cheeck various message parameters

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,12 +4,29 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  const { chats, activeChats, userName, messages } = props;
-  return (
-    <div>
-      ChatFeed
-    </div>
-  )
-}
+  const { chats, activeChat, userName, messages } = props;
+
+  // Finding active chat -- if chats exist, find activeChat;
+  const chat = chats && chats[activeChat];
+
+  // Rendering component messages
+  const renderMessages = () => {
+    // Assigning key from messages
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      // Assigning the message, finding it with key
+      const message = messages[key];
+
+      // Finding the last message sent
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+
+      // Checking if message is own user
+      const isMyMessage = userName === message.sender.username;
+    });
+  };
+
+  return <div>ChatFeed</div>;
+};
 
 export default ChatFeed;
