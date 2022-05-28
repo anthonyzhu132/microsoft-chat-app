6ae9225ebe83967c77ef28bb79f7bdf48adb7fc2
Anthony Zhu <anthonyzhu132@gmail.com>
2022-05-27 17:03:56 -0700

    added logic and basic form to send a message

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,11 +1,37 @@
-import React from "react"
+import React, { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
 
-const MessageForm = () => {
+const MessageForm = (props) => {
+  const { chatId, creds } = props;
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if(text.length > 0 ) {
+      sendMessage(creds, chatId, { text })
+    }
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
   return (
-    <div>
-      Message Form
-    </div>
-  )
-}
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message"
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
+  );
+};
 
 export default MessageForm;
